65bed2dea07049fdeb70c5e41a694fea514b543c
Deeksha Singh <78050476+dkshsingh@users.noreply.github.com>
2022-07-06 12:01:47 +0530

    Add files via upload

---
+++ wp.py
@@ -0,0 +1,14 @@
+from numpy import nanvar
+import pyautogui as pg 
+import time
+time.sleep(10)
+#deeksha
+for i in range(10):
+    pg.write('Hello , where are you')
+    pg.press('enter')
+    
+    
+    
+    
+   
+   
